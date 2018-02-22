c8526340feea79af8a7c9602bbace42a1708ce24
kde6260 <kde6260@gmail.com>
2018-02-22 23:07:05 +0900

    커스텀 필터, 로그아웃 세션 파기 추가

--- src/main/java/org/nexters/inhousekitchen/controller/HomeController.java
+++ src/main/java/org/nexters/inhousekitchen/controller/HomeController.java
@@ -1,28 +1,14 @@
 package org.nexters.inhousekitchen.controller;
 
-import java.io.IOException;
-import java.text.DateFormat;
-import java.util.Date;
-import java.util.Locale;
+
 
 import javax.annotation.Resource;
-import javax.servlet.http.HttpServletResponse;
 
-import org.nexters.inhousekitchen.dto.HostDTO;
 import org.nexters.inhousekitchen.service.SearchService;
-import org.nexters.inhousekitchen.service.SearchServiceimpl;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
+
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
-import io.swagger.annotations.Api;
-import io.swagger.annotations.ApiOperation;
 
 /**
  * Handles requests for the application home page.
